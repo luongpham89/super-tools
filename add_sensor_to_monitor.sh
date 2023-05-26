696ccbb4c907e9d543ca2c969cab9a5f0d5cbf66
Luong Pham <luongpham@playgroundvina.com>
2023-05-26 15:24:11 +0700

    .

--- add_sensor_to_monitor.sh
+++ add_sensor_to_monitor.sh
@@ -8,8 +8,8 @@ influxdb_host=$2
 influxdb_user=$3
 influxdb_pass=$4
 # Install telegraf
-wget -q https://repos.influxdata.com/influxdata-archive_compat.key -O influxdata-archive_compat.key
-echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
+wget -q https://repos.influxdata.com/influxdata-archive_compat.key -O /tmp/influxdata-archive_compat.key
+echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c /tmp/influxdata-archive_compat.key' | sha256sum -c && cat /tmp/influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
 echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
 sudo apt-get update -y && sudo apt-get install -y telegraf
 
