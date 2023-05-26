e7705a9f1c57ca462875a7efc308d88f2a92fd55
Luong Pham <luongpham@playgroundvina.com>
2023-05-26 14:23:56 +0700

    .

--- add_sensor_to_monitor.sh
+++ add_sensor_to_monitor.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 4 ]; then
     echo "Add hostname please"
     exit
 fi
 hostname=$1
-influxdb_pass=$2
+influxdb_host=$2
+influxdb_user=$3
+influxdb_pass=$4
 # Install telegraf
 wget -q https://repos.influxdata.com/influxdata-archive_compat.key -O influxdata-archive_compat.key
 echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
@@ -39,8 +41,8 @@ cat > /etc/telegraf/telegraf.d/custom.conf << EOF
 # Output Plugin InfluxDB
 [[outputs.influxdb]]
   database = "telegraf"
-  urls = [ "http://54.254.47.35:8086" ]
-  username = "telegraf2"
+  urls = [ "$influxdb_host" ]
+  username = "$influxdb_user"
   password = "$influxdb_pass"
   
 #Telegraf provides telegraf command to manage the configuration, 
